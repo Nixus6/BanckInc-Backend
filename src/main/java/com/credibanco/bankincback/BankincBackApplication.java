93ddea6f6536f0c98f4d1a6246fb1a466ecb5be9
Nicolas Moreno <nicolas007andres@hotmail.com>
2023-12-13 17:17:59 -0500

    chore(api)!: 🎉 archivos iniciales

---
+++ src/main/java/com/credibanco/bankincback/BankincBackApplication.java
@@ -0,0 +1,13 @@
+package com.credibanco.bankincback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankincBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankincBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/credibanco/bankincback/BankincBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.credibanco.bankincback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankincBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
