5e2070fa8731156ccd4daa4adf2f91929f8ed5c1
Nicolas Moreno <nicolas007andres@hotmail.com>
2023-12-19 20:51:54 -0500

    fix: 🥅 Manejo de excepciones al crear una nueva tarjeta

--- src/main/java/com/credibanco/bankincback/domain/service/CardService.java
+++ src/main/java/com/credibanco/bankincback/domain/service/CardService.java
@@ -1,13 +1,15 @@
 package com.credibanco.bankincback.domain.service;
 
 import com.credibanco.bankincback.domain.Card;
-import com.credibanco.bankincback.domain.dto.RechargeBalanceDto;
 import com.credibanco.bankincback.domain.repository.CardRepository;
 
 import com.credibanco.bankincback.persistence.entity.TypeCard;
 import jakarta.transaction.Transactional;
+import org.hibernate.TransactionException;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.DataAccessResourceFailureException;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -31,17 +33,26 @@ public class CardService {
         this.cardRepository = cardRepository;
     }
     public Card save(String cardId){
-
-        Random numAleatorio = new Random();
-        Long aleat1 = numAleatorio.nextLong(99999 - 10000 + 1) + 10000;
-        Long aleat2 = numAleatorio.nextLong(99999 - 10000 + 1) + 10000;
-        String idproducto = cardId + aleat1 + aleat2;
-        // Obtén el LocalDateTime actual
-        LocalDateTime fechaActual = LocalDateTime.now();
-        // Suma tres años
-        LocalDateTime fechaEnTresAnios = fechaActual.plusYears(3);
-        Card card = Card.builder().cardId(Long.parseLong(idproducto)).balance(0).quota(0).typeCard(TypeCard.CREDIT).expiration(fechaEnTresAnios).state(BLOQUED).build();
-        return this.cardRepository.save(card);
+        try {
+            Random numAleatorio = new Random();
+            long aleat1 = numAleatorio.nextLong(99999 - 10000 + 1) + 10000;
+            long aleat2 = numAleatorio.nextLong(99999 - 10000 + 1) + 10000;
+            String idproducto = cardId + aleat1 + aleat2;
+            // Obtén el LocalDateTime actual
+            LocalDateTime fechaActual = LocalDateTime.now();
+            // Suma tres años
+            LocalDateTime fechaEnTresAnios = fechaActual.plusYears(3);
+            Card card = Card.builder().cardId(Long.parseLong(idproducto)).balance(0).quota(0).typeCard(TypeCard.CREDIT).expiration(fechaEnTresAnios).state(BLOQUED).build();
+            return this.cardRepository.save(card);
+        }catch (DataAccessException e) {
+            // Manejar excepciones específicas de acceso a datos (por ejemplo, problemas de base de datos)
+            log.error("Error de acceso a datos al guardar la tarjeta", e);
+            throw new DataAccessResourceFailureException("Error al guardar la tarjeta", e);
+        } catch (Exception e) {
+            // Manejar otras excepciones genéricas que puedan ocurrir
+            log.error("Error al guardar la tarjeta", e);
+            throw new RuntimeException("Error al guardar la tarjeta", e);
+        }
 
     }
     @Transactional
